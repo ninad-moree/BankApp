285e25943eaaf9935fe401cc0b968050ccf4e217
Ninad More <ninadyogeshmore@gmail.com>
2023-10-28 18:45:33 +0530

    App Set Up Done

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from './style';
 
 const App = () => {
   return (
-    <div></div>
+    <div className="bg-primary w-full overflow-hidden">
+
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          NavBar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      
+    </div>
   )
 }
+
+export default App

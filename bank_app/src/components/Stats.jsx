f6e356df97ea28c7e231ce7524681a3bb6dde7db
Ninad More <ninadyogeshmore@gmail.com>
2023-10-29 18:42:00 +0530

    Business Section Done

--- bank_app/src/components/Stats.jsx
+++ bank_app/src/components/Stats.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { stats } from '../constants'
+import React from 'react';
+import { stats } from '../constants';
 import styles from '../style';
 
 const Stats = () => {

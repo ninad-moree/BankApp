e40fb6a6041a46414d4bd828ce6dcee33c5be60a
Ninad More <ninadyogeshmore@gmail.com>
2023-10-29 18:19:05 +0530

    Stats section done

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from './style';
 import {Navbar,Hero,Stats,Business,Billing,CardDeal,Testimonials,Client,CTA,Footer} from './components/index'
 
-
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">

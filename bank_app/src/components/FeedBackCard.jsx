0fe54df8eca5f0aee7ecfee79b02043f654d160c
Ninad More <ninadyogeshmore@gmail.com>
2023-10-30 10:16:37 +0530

    Footer Done

--- bank_app/src/components/FeedBackCard.jsx
+++ bank_app/src/components/FeedBackCard.jsx
@@ -3,7 +3,7 @@ import { quotes } from '../assets';
 
 const FeedBackCard = ({content, name , title,img}) => {
   return (
-    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[350px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
       <img src={quotes} alt="double quotes" className='w-[42px] h-[27px] object-contain' />
 
       <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>
